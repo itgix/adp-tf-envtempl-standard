fffd65d1c2b7460a30096202268805b22f3b2fc0
Mihail Vukadinoff <mihail.vukadinoff@itgix.com>
2024-02-19 15:23:22 +0200

    ext dns - add output

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "eks_cluster_arn" {
+  value = module.eks[0].eks_cluster_arn
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks[0].eks_cluster_endpoint
+}
+
+output "eks_cluster_certificate_authority_data" {
+  value = module.eks[0].eks_cluster_certificate_authority_data
+}
+
+#output "eks_cluster_version" {
+#  value = module.eks[0].eks_cluster_version
+#}
+
+output "eks_irsa_external_dns_arn" {
+  value = module.iam_assumable_role_external_dns.arn"
+}
