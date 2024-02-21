9d53dacbef1175f3e1608f6ae9ecda7b9780f13d
Mihail Vukadinoff <mihail.vukadinoff@itgix.com>
2024-02-21 17:18:00 +0200

    add cluster name output

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "eks_cluster_arn" {
   value = module.eks[0].eks_cluster_arn
 }
 
+output  "eks_cluster_name" {
+  value = module.eks[0].eks_cluster_id
+}
+
 output "eks_cluster_endpoint" {
   value = module.eks[0].eks_cluster_endpoint
 }
