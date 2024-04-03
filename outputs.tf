35a3b026eb61a18905858230113b0b36329f976d
Yordan Todorov <yordan.todorov@itgix.com>
2024-04-03 14:31:40 +0300

    fixing output sg

--- outputs.tf
+++ outputs.tf
@@ -24,5 +24,5 @@ output "eks_irsa_external_dns_arn" {
 
  output "node_security_group_id" {
    description = "ID of the node shared security group"
-   value       = module.eks.node_security_group_id
+   value       = module.eks[0].node_security_group_id
  }
