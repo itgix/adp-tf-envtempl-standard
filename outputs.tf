d440fbe15ab3065d86be3b28906fcaf84de918e0
Tyurkiyan Mehmedova <tyurkiyan.mehmedova@itgix.com>
2024-04-23 13:52:49 +0300

    adding prefix ecr_ on outputs

--- outputs.tf
+++ outputs.tf
@@ -67,22 +67,22 @@ output "waf_webacl_arn" {
 
 # ECR
 
-output "repository_name" {
+output "ecr_repository_name" {
   description = "Name of the repository"
   value       = module.ecr.repository_name
 }
 
-output "repository_arn" {
+output "ecr_repository_arn" {
   description = "Full ARN of the repository"
   value       = module.ecr.repository_arn
 }
 
-output "repository_registry_id" {
+output "ecr_repository_registry_id" {
   description = "The registry ID where the repository was created"
   value       = module.ecr.repository_registry_id
 }
 
-output "repository_url" {
+output "ecr_repository_url" {
   description = "The URL of the repository (in the form `aws_account_id.dkr.ecr.region.amazonaws.com/repositoryName`)"
   value       = module.ecr.repository_url
 }
