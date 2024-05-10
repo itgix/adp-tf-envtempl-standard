13e978bd4024ca1ed7a9a323492eeae9ea94b9e9
Mihail Vukadinoff <mihail.vukadinoff@itgix.com>
2024-05-10 19:15:58 +0300

    set by default to generate a wildcard cert

--- acm-certificate.tf
+++ acm-certificate.tf
@@ -1,7 +1,7 @@
 module "acm" {
   count  = var.acm_certificate_enable? 1 : 0
   source = "git::ssh://git@gitlab.itgix.com/educatedguessteam/tf-modules/tf-module-acm.git?ref=main"
-
-  domain_name        = var.dns_main_domain
+  # Configured for wildcard certificate
+  domain_name        = "*.${var.dns_main_domain}"
   r53_zone_id        = var.dns_hosted_zone
 }
