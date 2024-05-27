28d76d7d5939cd37a0bc836e8719ae531543953b
Daniel Milanov <daniel.milanov@itgix.com>
2024-05-27 15:18:50 +0300

    Reference acm module from different place

--- acm-certificate.tf
+++ acm-certificate.tf
@@ -1,6 +1,6 @@
 module "acm" {
   count  = var.acm_certificate_enable? 1 : 0
-  source = "git::ssh://git@https://gitlab.itgix.com/rnd/app-platform/iac-modules/tf-module-acm.git?ref=main"
+  source = "git::ssh://git@gitlab.itgix.com/rnd/app-platform/iac-modules/tf-module-acm.git?ref=main"
   # Configured for wildcard certificate
   domain_name        = "*.${var.dns_main_domain}"
   r53_zone_id        = var.dns_hosted_zone
