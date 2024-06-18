ad143daf432ad72ecce36e12d2cc69cc7059d9c6
Mihail Vukadinoff <mihail.vukadinoff@itgix.com>
2024-06-18 10:08:48 +0300

    elasticache KMS key add to external secrets role

--- locals.tf
+++ locals.tf
@@ -36,7 +36,8 @@ locals {
   eks_name = "eks-${local.aws_regions_short[var.region]}-${var.environment}-${var.project_name}"
 
   secrets_kms_key_arns = [
-    module.rds_maindb[0].rds_credentials_kms_key_arn
+    module.rds_maindb[0].rds_credentials_kms_key_arn,
+    module.elasticache[0].redis_secret_kms_key_arn
   ]
 
   karpenter_queue_name           = "queue-${var.region}-${var.environment}-karpenter"
