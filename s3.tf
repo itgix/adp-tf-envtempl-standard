1e8da0671231d7185d4c2f3538ac212347feab5f
Tsvetana Kanzova <tsvetana.kanzova@itgix.com>
2025-02-07 15:21:17 +0200

    Modified again the S3 modul ajusting bucket_name_suffix variable

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 module "s3_bucket" {
   count = var.s3_create ? 1 : 0
-  source  = "git::git@github.com:itgix/tf-module-s3.git?ref= v1.1.2"
+  source  = "git::git@github.com:itgix/tf-module-s3.git?ref=v1.1.4"
   
 
   region          = var.region
