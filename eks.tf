aa15be8ba38d7ccfb8e13f1ba24bb6ecaabb4b36
Hristiyan Tonev <41945933+hvtonev@users.noreply.github.com>
2025-03-14 17:56:01 +0200

    select the actual eks module tag

--- eks.tf
+++ eks.tf
@@ -1,5 +1,5 @@
 module "eks" {
-  source = "git::git@github.com:itgix/tf-module-eks.git?ref=eks-karpenter-20.31.6-upgrade"
+  source = "git::git@github.com:itgix/tf-module-eks.git?ref=v1.1.0"
   count  = var.provision_eks ? 1 : 0
 
   providers = {
