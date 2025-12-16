232070052d6b687ea5d5c774326d3f9b1fd3f389
Tsvetana Kanzova <tsvetana.kanzova@itgix.com>
2025-12-16 11:39:14 +0200

    Added specific version for the aws provider in order to prevent the bugs from version 5.100.0

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.93.0, < 5.95.0"
+      version = "= 5.93.0"
     }
     random = {
       source  = "hashicorp/random"
