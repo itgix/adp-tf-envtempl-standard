dd32085ee56f1059bde7f3d49df3f0fb73a1c181
Ivan Roussev <ivan.roussev12@gmail.com>
2026-01-10 14:55:48 +0200

    fix: making changes to awsm-passgen repo to incorporate values to secrets

--- custom_secrets.tf
+++ custom_secrets.tf
@@ -1,5 +1,5 @@
 module "custom_secrets_password_module" {
-  source = "git@github.com:itgix/tf-module-awssm-passgen.git?ref=v1.0.0"
+  source = "git@github.com:iroussev-itgix/tf-module-awssm-passgen.git?ref=feat/support-custom-secret-values"
 
   custom_secrets = var.custom_secrets
 
