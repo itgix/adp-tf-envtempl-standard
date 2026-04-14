46361307102867ca5eac2d44d31c297820c68dde
Ralitsa Dimitrova <ralitsa.dimitrova@itgix.com>
2026-04-14 10:10:03 +0300

    new release for the tf-module-s3

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 module "s3_bucket" {
   count  = var.s3_create ? 1 : 0
-  source = "git::git@github.com:itgix/tf-module-s3.git?ref=bucket-policies"
+  source = "git::git@github.com:itgix/tf-module-s3.git?ref=v1.1.10"
 
 
   region       = var.region
