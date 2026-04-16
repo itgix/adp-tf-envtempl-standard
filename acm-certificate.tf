cda3f746d7410186e9f2f073c939578d6b2fab95
Victoria Dimitrova <victoria.dimitrova@itgix.com>
2026-04-16 11:43:33 +0300

    Fix wildcard

--- acm-certificate.tf
+++ acm-certificate.tf
@@ -5,7 +5,7 @@ module "acm" {
   # To support previous setup with single domain;
   # Still need to define it in the config file for the argo and infra facts setup;
   r53_zone_id = var.dns_hosted_zone
-  domain_name = var.dns_main_domain
+  domain_name = "*.${var.dns_main_domain}"
 
   # Use this one for multiple domains;
   # If this one is used, the above one is not active for the terraform
