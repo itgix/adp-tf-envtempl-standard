ca7ad6762dec85d210b8940acc211ab7c5c508e7
Victoria Dimitrova <victoria.dimitrova@itgix.com>
2026-04-29 15:26:44 +0300

    Manage multiple IP prefix lists and rules

--- waf.tf
+++ waf.tf
@@ -1,48 +1,47 @@
 module "wafv2_application" {
-  source                           = "git::git@github.com:itgix/tf-module-wafv2.git?ref=v7"
-  waf_enabled                      = var.application_waf_enabled
-  project                          = var.project_name
-  env                              = var.environment
-  aws_region                       = var.region
-  pid                              = var.project_name
-  web_acl_scope                    = "REGIONAL"
-  waf_attachment_type              = "application"          # appended to WebACL and Log Group name
-  waf_default_action               = var.waf_default_action # allow or block - default action if a request doesn't match any rule
-  web_acl_cloudwatch_enabled       = var.waf_webacl_cloudwatch_enabled
-  sampled_requests_enabled         = var.waf_sampled_requests_enabled
-  aws_waf_logging_enabled          = var.application_waf_enabled ? var.waf_logging_enabled : false
-  waf_log_retention_days           = var.waf_log_retention_days
-  aws_managed_waf_rule_groups      = var.aws_managed_waf_rule_groups          # list of AWS managed security rules to be enabled
-  custom_managed_waf_rule_groups   = var.custom_managed_waf_rule_groups       # list of custom managed security rules to be enabled
-  custom_rules                     = var.waf_custom_rules                     # list of custom rules to be enabled
-  ip_whitelist_prefixes            = var.waf_ip_whitelist_prefixes            # list of IP prefixes to whitelist
-  ip_whitelist_rule_priority       = var.waf_ip_whitelist_rule_priority       # priority of the IP whitelist rule
-  ip_whitelist_forwarded_ip_config = var.waf_ip_whitelist_forwarded_ip_config # forwarded IP config for the IP whitelist
+  source                         = "git::git@github.com:itgix/tf-module-wafv2.git?ref=ip-prefix"
+  waf_enabled                    = var.application_waf_enabled
+  project                        = var.project_name
+  env                            = var.environment
+  aws_region                     = var.region
+  pid                            = var.project_name
+  web_acl_scope                  = "REGIONAL"
+  waf_attachment_type            = "application"          # appended to WebACL and Log Group name
+  waf_default_action             = var.waf_default_action # allow or block - default action if a request doesn't match any rule
+  web_acl_cloudwatch_enabled     = var.waf_webacl_cloudwatch_enabled
+  sampled_requests_enabled       = var.waf_sampled_requests_enabled
+  aws_waf_logging_enabled        = var.application_waf_enabled ? var.waf_logging_enabled : false
+  waf_log_retention_days         = var.waf_log_retention_days
+  aws_managed_waf_rule_groups    = var.aws_managed_waf_rule_groups    # list of AWS managed security rules to be enabled
+  custom_managed_waf_rule_groups = var.custom_managed_waf_rule_groups # list of custom managed security rules to be enabled
+  custom_rules                   = var.waf_custom_rules               # list of custom rules to be enabled
+  ip_whitelist_prefixes          = var.waf_ip_whitelist_prefixes      # list of IP prefixes to whitelist
+  ip_prefix_sets                 = var.waf_ip_prefix_sets             # Define IP prefix sets
+  ip_prefix_rules                = var.waf_ip_prefix_rules            # # Define IP prefix rules
 }
 
 module "wafv2_cloudfront" {
-  source      = "git::git@github.com:itgix/tf-module-wafv2.git?ref=v7"
+  source      = "git::git@github.com:itgix/tf-module-wafv2.git?ref=ip-prefix"
   waf_enabled = var.cloudfront_waf_enabled
 
   providers = {
     aws = aws.virginia
   }
 
-  project                          = var.project_name
-  env                              = var.environment
-  aws_region                       = var.region
-  pid                              = var.project_name
-  web_acl_scope                    = "CLOUDFRONT"
-  waf_attachment_type              = "cloudfront" # appended to WebACL and Log Group name
-  waf_default_action               = var.waf_default_action
-  web_acl_cloudwatch_enabled       = var.waf_webacl_cloudwatch_enabled
-  sampled_requests_enabled         = var.waf_sampled_requests_enabled
-  aws_waf_logging_enabled          = var.cloudfront_waf_enabled ? var.waf_logging_enabled : false
-  waf_log_retention_days           = var.waf_log_retention_days
-  aws_managed_waf_rule_groups      = var.aws_managed_waf_rule_groups          # list of AWS managed security rules to be enabled
-  custom_managed_waf_rule_groups   = var.custom_managed_waf_rule_groups       # list of custom managed security rules to be enabled
-  custom_rules                     = var.waf_custom_rules                     # list of custom rules to be enabled
-  ip_whitelist_prefixes            = var.waf_ip_whitelist_prefixes            # list of IP prefixes to whitelist
-  ip_whitelist_rule_priority       = var.waf_ip_whitelist_rule_priority       # priority of the IP whitelist rule
-  ip_whitelist_forwarded_ip_config = var.waf_ip_whitelist_forwarded_ip_config # forwarded IP config for the IP whitelist
+  project                        = var.project_name
+  env                            = var.environment
+  aws_region                     = var.region
+  pid                            = var.project_name
+  web_acl_scope                  = "CLOUDFRONT"
+  waf_attachment_type            = "cloudfront" # appended to WebACL and Log Group name
+  waf_default_action             = var.waf_default_action
+  web_acl_cloudwatch_enabled     = var.waf_webacl_cloudwatch_enabled
+  sampled_requests_enabled       = var.waf_sampled_requests_enabled
+  aws_waf_logging_enabled        = var.cloudfront_waf_enabled ? var.waf_logging_enabled : false
+  waf_log_retention_days         = var.waf_log_retention_days
+  aws_managed_waf_rule_groups    = var.aws_managed_waf_rule_groups    # list of AWS managed security rules to be enabled
+  custom_managed_waf_rule_groups = var.custom_managed_waf_rule_groups # list of custom managed security rules to be enabled
+  custom_rules                   = var.waf_custom_rules               # list of custom rules to be enabled
+  ip_prefix_sets                 = var.waf_ip_prefix_sets             # Define IP prefix sets
+  ip_prefix_rules                = var.waf_ip_prefix_rules            # # Define IP prefix rules
 }
