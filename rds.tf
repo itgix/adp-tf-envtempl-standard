ad6d518d443f9941038239c465292d3cad6b0a08
Ralitsa Dimitrova <ralitsa.dimitrova@itgix.com>
2026-07-09 13:40:30 +0300

    new tag for the tf-module-rds

--- rds.tf
+++ rds.tf
@@ -3,8 +3,7 @@ module "rds_maindb" {
 
   depends_on = [module.common_vpc]
 
-  #source = "git::https://github.com/itgix/tf-module-rds.git?ref=v1.0.8"
-  source = "git::https://github.com/itgix/tf-module-rds.git?ref=failover_priority"
+  source = "git::https://github.com/itgix/tf-module-rds.git?ref=v1.0.9"
 
   environment = var.environment
 
