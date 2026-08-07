d89e655ef78bbf868b8c4cb8ec16cd014e04ce4f
galeksandrov22 <georgi.aleksandrov@itgix.com>
2026-08-07 11:32:38 +0300

    revert: remove rds_database_insights_mode (unsupported in tf-module-rds v1.0.9)

--- rds.tf
+++ rds.tf
@@ -54,4 +54,6 @@ module "rds_maindb" {
   rds_db_instance_parameters = var.rds_db_instance_parameters
 
   failover_priority = var.rds_failover_priority
+
+  rds_performance_retention = var.rds_performance_retention
 }
