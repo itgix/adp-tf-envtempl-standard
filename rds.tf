bb1874aeba5bf55826276e9d65aaa7dd022d7aac
Victoria Dimitrova <victoria.dimitrova@itgix.com>
2026-09-11 14:55:15 +0300

    update to tag 1.0.12

--- rds.tf
+++ rds.tf
@@ -3,7 +3,7 @@ module "rds_maindb" {
 
   depends_on = [module.common_vpc]
 
-  source = "git::https://github.com/itgix/tf-module-rds.git?ref=maintenance_window"
+  source = "git::https://github.com/itgix/tf-module-rds.git?ref=v1.0.12"
 
   environment = var.environment
 
